cb4fd2183d1e5deda3cc6e16a0ce5e6dac4db60a
André Wagner <andre.nho@gmail.com>
2019-11-23 03:00:17 +0000

    .

--- k8s/backend/main.go
+++ k8s/backend/main.go
@@ -12,6 +12,8 @@ import (
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 }
 
 //
@@ -95,7 +97,7 @@ func recipeHandler(w http.ResponseWriter, r *http.Request) {
       "medium",
       []Ingredient {
         { "Brewed (strong)", 0.5, "coffee", "#610B0B", 3.0, 2, false },
-        { "Mild", 0.5, "liquid", "#FAFAFA", 2.0, 2, false },
+        { "Milk", 0.5, "liquid", "#FAFAFA", 2.0, 2, false },
       },
       5.0,
     },
@@ -169,7 +171,77 @@ func ingredientHandler(w http.ResponseWriter, r *http.Request) {
 // orders
 //
 
+func insertOrderInDatabase(db *sql.DB, order Order) error {
+  tx, err := db.Begin()
+  if err != nil {
+    return err
+  }
+  defer tx.Rollback()
+
+  // orders
+  stmt, err := tx.Prepare(`INSERT INTO orders (name, email, address, city, state, zip, delivery_cost, tax_cost, total )
+                                VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+  if err != nil {
+    return err
+  }
+  d := order.DeliveryAddress
+  result, err := stmt.Exec(d.Name, d.Email, d.Address, d.City, d.State, d.Zip, order.DeliveryCost, order.TaxCost, order.Total)
+  if err != nil {
+    return err
+  }
+  id, err := result.LastInsertId()
+  if err != nil {
+    return err
+  }
+  stmt.Close()
+
+  // items
+  stmt, err = tx.Prepare(`INSERT INTO order_items (order_id, num, name, description, size, total_cost)
+                                VALUES ( ?, ?, ?, ?, ?, ? )`)
+  if err != nil {
+    return err
+  }
+  for i, item := range order.Items {
+    size := "X"
+    switch item.Size {
+    case "small": size = "S"
+    case "medium": size = "M"
+    case "large": size = "L"
+    }
+    _, err = stmt.Exec(id, i+1, item.Name, item.Description, size, item.TotalCost)
+    if err != nil {
+      return err
+    }
+
+    // ingredients
+    stmt_i, err := tx.Prepare(`INSERT INTO item_ingredients (order_id, item_num, ingredient_id)
+                                    VALUES (?, ?, (SELECT id FROM ingredients WHERE name = ?))`)
+    if err != nil {
+      return err
+    }
+    for _, ing := range item.Ingredients {
+      _, err = stmt_i.Exec(id, i+1, ing.Name)
+      if err != nil {
+        return err
+      }
+    }
+    stmt_i.Close()
+  }
+  stmt.Close()
+
+  err = tx.Commit()
+  if err != nil {
+    return err
+  }
+
+  return nil
+}
+
 func orderHandler(w http.ResponseWriter, r *http.Request) {
+  enableCors(&w)
+  if r.Method == http.MethodOptions {
+    return
+  }
   if r.Method != http.MethodPost {
     http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
   }
@@ -178,13 +250,9 @@ func orderHandler(w http.ResponseWriter, r *http.Request) {
   var order Order
   err:= decoder.Decode(&order)
   if err != nil {
-    panic(err)
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }
-
-  // TODO - insert in database
-  log.Println(order)
 
   db, err := openDatabase()
   if err != nil {
@@ -193,7 +261,13 @@ func orderHandler(w http.ResponseWriter, r *http.Request) {
   }
   defer db.Close()
 
-  enableCors(&w)
+  err = insertOrderInDatabase(db, order)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  fmt.Fprintln(w, "ok")
 }
 
 // 
@@ -204,6 +278,6 @@ func main() {
   http.HandleFunc("/healthz", health)
   http.HandleFunc("/ingredients/", ingredientHandler)
   http.HandleFunc("/recipes/global/", recipeHandler)
-  http.HandleFunc("/cart/", orderHandler)
+  http.HandleFunc("/cart", orderHandler)
   log.Fatal(http.ListenAndServe(":8888", nil))
 }
