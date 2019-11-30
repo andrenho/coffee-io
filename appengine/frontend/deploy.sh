9bd037053e1b83d1a20418e4c86715d0c59b6764
André Wagner <andre.nho@gmail.com>
2019-11-30 13:11:26 +0000

    .

--- appengine/frontend/deploy.sh
+++ appengine/frontend/deploy.sh
@@ -4,6 +4,7 @@ set -x
 
 BUCKET=gs://www.appengine.coffee.gamesmith.uk
 
+# compile application
 pushd .
 cd ../../frontend
 yarn build
@@ -11,11 +12,20 @@ popd
 
 cp -R ../../frontend/build .
 
+# create bucket
 gsutil mb $BUCKET
+
+# clear pre-exiting files on bucket
 gsutil -m rm -rf $BUCKET/*
+
+# copy files to bucket
 cd build
 gsutil -m cp -r . $BUCKET/
 cd ..
+rm -rf build
+
+# make bucket public
 gsutil iam ch allUsers:objectViewer $BUCKET
 
-rm -rf build
+# set index page
+gsutil web set -m index.html -e index.html $BUCKET
